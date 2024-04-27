1f690d2a23cec73d54238c600f418a24ede2031a
Xue Yang <74811377+yxSMU@users.noreply.github.com>
2024-04-27 14:37:20 +0800

    Add files via upload

---
+++ DatasetMakeCode/README.md
@@ -0,0 +1,2 @@
+
+Code in this package for making datasets. (1)File dataset_blodigit_make aims to make the bold&origin dataset. (2)File Mnist-C-Master aims to acquire the noise&origin dataset and edge&origin dataset.
---
+++ DatasetMakeCode/dataset_blodigit_make/blod_train.py
@@ -0,0 +1,37 @@
+import torch
+from torchvision import datasets, transforms
+import cv2
+from torchvision.utils import save_image
+import numpy as np
+import os
+
+torch.manual_seed(42)
+
+transform = transforms.Compose([
+    transforms.ToTensor(),
+])
+
+mnist_dataset = datasets.MNIST(root='./data', train=True, download=True, transform=transform)
+
+filtered_indices = [i for i in range(len(mnist_dataset)) if mnist_dataset.targets[i] == 6][1000:1250]
+
+os.makedirs(r'E:\ACMMM\YJC\YJC\Data\Dilate\6\A', exist_ok=True)
+os.makedirs(r'E:\ACMMM\YJC\YJC\Data\Dilate\6\B', exist_ok=True)
+
+
+kernel = np.ones((2, 2), np.uint8)
+for i in filtered_indices:
+    image, label = mnist_dataset[i]
+
+    image_np = (image.squeeze().numpy() * 255).astype(np.uint8) 
+   
+    torch_image = torch.from_numpy(image_np / 255.0).unsqueeze(0)
+
+    original_path = r'E:\ACMMM\YJC\YJC\Data\Dilate\6\B\origin_{}.png'.format(i)
+    save_image(torch_image, original_path)
+
+    dilated_image = cv2.dilate(image_np, kernel, iterations=1)
+    torch_image_binarized=torch.from_numpy(dilated_image / 255.0).unsqueeze(0)
+    dilate_path  = r'E:\ACMMM\YJC\YJC\Data\Dilate\6\A\dilate{}.png'.format(i)
+    save_image(torch_image_binarized ,dilate_path)
+
